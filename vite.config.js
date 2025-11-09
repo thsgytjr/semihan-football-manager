6e3a55fd02ffd40e9f71ad0996bee3ff0c9229db
David Son <davidson@Davids-MacBook-Air.local>
2025-11-08 19:42:57 -0600

    Fix deployment error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { execSync } from 'child_process'
-import pkg from './package.json' assert { type: 'json' }
+import { readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'))
 
 // git 커밋 해시 가져오기
 let commitHash = 'dev'
