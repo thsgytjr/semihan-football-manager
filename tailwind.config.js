9e507cbda6f928ca90829334c47aa02feaa034de
David Son <davidson@Davids-MacBook-Air.local>
2025-12-09 11:09:05 -0600

    Update tailwind with postCss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-    darkMode: false, // 다크모드 비활성화
     theme: { extend: {} },
     plugins: [],
   }
